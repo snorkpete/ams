b109bdf2f90ac9e5e91e8282fec907173a4fd57e
Kion Stephen <kion.stephen@gmail.com>
2013-08-25 13:57:24 -0400

    Dont allow signups. Users must be added by admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 end
