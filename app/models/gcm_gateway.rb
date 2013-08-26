fbcdfee0a313328a49dd1d0d75092ce9055d135d
Kion Stephen <kion.stephen@gmail.com>
2013-08-25 20:01:49 -0400

    Send gcm messages to all registered devices

--- app/models/gcm_gateway.rb
+++ app/models/gcm_gateway.rb
@@ -5,7 +5,7 @@ class GcmGateway
   def send_notification(announcement)
     GCM.key = "AIzaSyB4o0cy3akF1BViFaDE1Ld-kN_0kqlGEQ0"
 
-    regid = 'APA91bE1DYm3n09KQQ9MGSkLJYeXt5lKMEhIYcPkYuXWw7jwb-24BkF7g8oI-IhyPQgznr61vBgcYAox_pLV32zH_OOK7E75XR-zRUIpNf7A5tGuEovm_ldOItX2JPyZAwcmcCg3vtxIeZfUXPntp_2qzMDZxg8IvQ'
+    #regid = 'APA91bE1DYm3n09KQQ9MGSkLJYeXt5lKMEhIYcPkYuXWw7jwb-24BkF7g8oI-IhyPQgznr61vBgcYAox_pLV32zH_OOK7E75XR-zRUIpNf7A5tGuEovm_ldOItX2JPyZAwcmcCg3vtxIeZfUXPntp_2qzMDZxg8IvQ'
 
     announcement = { 
                      announcement_text: announcement.description, 
@@ -13,7 +13,15 @@ class GcmGateway
                      announcement_id:announcement.id
                     }
 
-    device_ids = [regid]
+    # send the GCM message to groups of 1000 devices at a time
+    Device.find_in_batches(:batch_size => 1000) do |devices|
+      device_ids = devices.map(&:regid)
+      GCM.send_notification(device_ids, announcement)
+    end
+    
+    #TODO: this is a performance bug waiting to happen.
+    # must be converted to find in batches
+    #device_ids = Device.all.select('regid').to_a
 
     #destination = ["device1", "device2", "device3"]
     # can be an string or an array of strings containing the regIds of the devices you want to send
@@ -23,6 +31,6 @@ class GcmGateway
 
     #GCM.send_notification( destination, data )
 
-    GCM.send_notification(device_ids, announcement)
+    #GCM.send_notification(device_ids, announcement)
   end
 end
