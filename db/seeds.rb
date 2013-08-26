9a3ec2db6ca0b1f85bee773b80a40a3b54a92061
Kion Stephen <kion.stephen@gmail.com>
2013-08-25 20:40:41 -0400

    fix the db/seeds function

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,7 @@
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name(role)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.add_role :admin
-
+Role.find_or_create_by_name('admin')
 
 puts 'Add jonaski users'
 user = User.find_or_create_by_email name: 'Kion Stephen', email:'kion.stephen@jonaski.com',
